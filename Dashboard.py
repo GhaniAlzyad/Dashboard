6e4e51cf3d856880b201e6add99254367b365432
GhaniAlzyad <121442547+GhaniAlzyad@users.noreply.github.com>
2026-01-18 15:35:13 +0000

    Dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,210 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
 
+sns.set(style='dark')
+
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_purchase_timestamp').agg({
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "price": "revenue"
+    }, inplace=True)
+    return daily_orders_df
+
+def create_sum_order_items_df(df):
+    sum_order_items_df = df.groupby("product_category_name_english").order_id.nunique().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+def create_by_state_df(df):
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    return bystate_df
+
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_unique_id", as_index=False).agg({
+        "order_purchase_timestamp": "max", 
+        "order_id": "nunique", 
+        "price": "sum" 
+    })
+    rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+    
+    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+    recent_date = df["order_purchase_timestamp"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+    
+    return rfm_df
+
+# ====================================================================
+# LOAD DATA
+# ====================================================================
+
+# Mengambil data dari file csv yang sudah dibersihkan
+all_df = pd.read_csv("main_data.csv")
+
+# Memastikan kolom tanggal bertipe datetime
+datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+all_df.sort_values(by="order_purchase_timestamp", inplace=True)
+all_df.reset_index(inplace=True)
+
+# ====================================================================
+# FILTER SIDEBAR
+# ====================================================================
+
+min_date = all_df["order_purchase_timestamp"].min()
+max_date = all_df["order_purchase_timestamp"].max()
+
+with st.sidebar:
+    # Menambahkan logo perusahaan (Opsional)
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+# Filter data utama
+main_df = all_df[(all_df["order_purchase_timestamp"] >= str(start_date)) & 
+                (all_df["order_purchase_timestamp"] <= str(end_date))]
+
+# ====================================================================
+# MENYIAPKAN DATAFRAME VISUALISASI
+# ====================================================================
+
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bystate_df = create_by_state_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+# ====================================================================
+# DASHBOARD PAGE
+# ====================================================================
+
+st.header('Dicoding E-Commerce Dashboard :sparkles:')
+
+# --- 1. DAILY ORDERS ---
+st.subheader('Daily Orders')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total Orders", value=total_orders)
+
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_purchase_timestamp"],
+    daily_orders_df["order_count"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+# --- 2. PRODUCT PERFORMANCE ---
+st.subheader("Best & Worst Performing Product")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 6))
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+# Best Product
+sns.barplot(x="order_id", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel(None)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=15)
+ax[0].tick_params(axis ='y', labelsize=12)
+
+# Worst Product
+sns.barplot(x="order_id", y="product_category_name_english", data=sum_order_items_df.sort_values(by="order_id", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel(None)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=15)
+ax[1].tick_params(axis='y', labelsize=12)
+
+st.pyplot(fig)
+
+# --- 3. CUSTOMER DEMOGRAPHICS ---
+st.subheader("Customer Demographics")
+
+fig, ax = plt.subplots(figsize=(20, 10))
+colors_ = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    x="customer_count", 
+    y="customer_state",
+    data=bystate_df.sort_values(by="customer_count", ascending=False).head(8),
+    palette=colors_,
+    ax=ax
+)
+ax.set_title("Number of Customer by States", loc="center", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+# --- 4. RFM PARAMETERS ---
+st.subheader("Best Customer Based on RFM Parameters")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    avg_recency = round(rfm_df.recency.mean(), 1)
+    st.metric("Average Recency (days)", value=avg_recency)
+
+with col2:
+    avg_frequency = round(rfm_df.frequency.mean(), 2)
+    st.metric("Average Frequency", value=avg_frequency)
+
+with col3:
+    avg_monetary = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
+    st.metric("Average Monetary", value=avg_monetary)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(30, 6))
+colors = ["#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4", "#72BCD4"]
+
+# Recency
+sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel(None)
+ax[0].set_title("By Recency (Days)", loc="center", fontsize=18)
+ax[0].tick_params(axis ='x', labelsize=15)
+ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=45, ha='right')
+
+# Frequency
+sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel(None)
+ax[1].set_title("By Frequency", loc="center", fontsize=18)
+ax[1].tick_params(axis='x', labelsize=15)
+ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45, ha='right')
+
+# Monetary
+sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+ax[2].set_ylabel(None)
+ax[2].set_xlabel(None)
+ax[2].set_title("By Monetary", loc="center", fontsize=18)
+ax[2].tick_params(axis='x', labelsize=15)
+ax[2].set_xticklabels(ax[2].get_xticklabels(), rotation=45, ha='right')
+
+st.pyplot(fig)
